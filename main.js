e4a803f59873969e6e3818823b6c80bb32c74bf4
DESKTOP-2I00R5B\Marie <mmarie.laporte@gmail.com>
2026-09-11 09:55:15 +0200

    Fix Linux title video GPU crash in play tests

--- main.js
+++ main.js
@@ -986,7 +986,12 @@ ipcMain.handle('open-game', async (event, location, battleTest) => {
 			await closeGameProcess();
 			const args = process.defaultApp ? [__filename] : [];
 			if (process.platform === 'linux') {
-				args.push('--no-sandbox', '--force-device-scale-factor=1', '--ozone-platform=x11');
+				args.push(
+					'--no-sandbox',
+					'--force-device-scale-factor=1',
+					'--ozone-platform=x11',
+					'--disable-accelerated-video-decode',
+				);
 			}
 			args.push(
 				'--rpm-game-test',
